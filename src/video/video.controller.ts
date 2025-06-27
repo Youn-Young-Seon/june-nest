071278ad8ae004278ae79c1e1e6cb7837438041c
YounYoungSeon <dbsyys@s3i.co.kr>
2025-06-27 16:54:21 +0900

    파일 경로 조회 추가 파일 스트리밍 로직 서비스로 이동

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -3,7 +3,6 @@ import { VideoService } from './video.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname, join } from 'path';
-import { createReadStream } from 'fs';
 import { Response } from 'express';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { CurrentUser, Public } from 'src/auth/decorators';
@@ -61,33 +60,8 @@ export class VideoController {
 
   @Get('stream/:idx')
   @Public()
-  async streamVideo(@Param('idx') filename: string, @Res() res: Response) {
-    const videoPath = join(process.cwd(), 'public', 'temp', filename);
-    const stat = await this.videoService.getVideoStat(videoPath);
-    const fileSize = stat.size;
-    const range = res.req.headers.range;
-
-    if (range) {
-      // 스트리밍(부분 전송) 지원
-      const parts = range.replace(/bytes=/, '').split('-');
-      const start = parseInt(parts[0], 10);
-      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-      const chunkSize = end - start + 1;
-      const file = createReadStream(videoPath, { start, end });
-      res.writeHead(206, {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunkSize,
-        'Content-Type': 'video/mp4',
-      });
-      file.pipe(res);
-    } else {
-      // 전체 파일 전송
-      res.writeHead(200, {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      });
-      createReadStream(videoPath).pipe(res);
-    }
+  async streamVideo(@Param('idx') idx: string, @Res() res: Response) {
+    const findVideo = await this.videoService.findOne(+idx);
+    this.videoService.streamingVideo(findVideo.filePath, res);
   }
 }
--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -3,13 +3,15 @@ import { CreateVideoDto } from './dto/create-video.dto';
 import { stat } from 'fs/promises';
 import { PrismaService } from 'src/common/prisma.service';
 import { User } from '@prisma/client';
+import { createReadStream } from 'fs';
+import { Response } from 'express';
 
 @Injectable()
 export class VideoService {
 
   private readonly logger = new Logger(VideoService.name);
 
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async createVideo(file: Express.Multer.File, createVideoDto: CreateVideoDto, user: User) {
     return this.prisma.video.create({
@@ -60,11 +62,65 @@ export class VideoService {
     return 'This action adds a new video';
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} video`;
+  async findOne(id: number) {
+    return await this.prisma.video.findUnique({
+      select: {
+        idx: true,
+        title: true,
+        description: true,
+        fileName: true,
+        filePath: true,
+        mimeType: true,
+        size: true,
+        thumbnailPath: true,
+        createdAt: true,
+        updatedAt: true,
+        uploadedBy: {
+          select: {
+            idx: true,
+            name: true,
+            email: true,
+          }
+        }
+      },
+      where: {
+        idx: id
+      }
+    });
   }
 
-  async getVideoStat(path: string) {
-    return stat(path); // 파일의 Stat 객체 반환 (size, mtime 등 포함)
+  async streamingVideo(path: string, res: Response<string, any>) {
+    const stat = await this.getStat(path);
+
+    const fileSize = stat.size;
+    const range = res.req.headers.range;
+
+    if (range) {
+      const parts = range.replace(/bytes=/, '').split('-');
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      const chunkSize = end - start + 1;
+      const file = createReadStream(path, { start, end });
+
+      res.writeHead(206, {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunkSize,
+        'Content-Type': 'video/mp4',
+      });
+
+      file.pipe(res);
+    } else {
+      res.writeHead(200, {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      });
+
+      createReadStream(path).pipe(res);
+    }
+  }
+
+  async getStat(videoPath: string) {
+    return stat(videoPath);
   }
 }
